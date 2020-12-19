a634e41f1682a0276850906061983ec8557e069b
jojotenya <jojotenya@gmail.com>
2020-12-20 02:36:03 +0800

    delete dependendson

--- deleteStack.sh
+++ deleteStack.sh
@@ -1,3 +1,4 @@
 aws s3 rm s3://nba.assets --recursive
 aws s3 rm s3://nba.website --recursive
+aws cloudformation delete-stack --stack-name NBALambda 
 aws cloudformation delete-stack --stack-name NBA 
